f8627e97e276b2463be5a08dad07430ea40ffba3
yuichiro12 <talcnrhyme@gmail.com>
2015-06-29 11:50:48 +0900

    make plate

---
+++ index3.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="test3.css">
+</head>
+<body>
+	<table class="table">
+		<tbody class="table">
+			<tr>
+				<td>
+					<table class="table">
+						<tbody class="table">
+							<tr>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+							</tr>
+							<tr>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+							</tr>
+							<tr>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+							</tr>
+						</tbody>
+					</table>
+				</td>
+				<td>
+					<table class="table">
+						<tbody class="table">
+							<tr>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+							</tr>
+							<tr>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+							</tr>
+							<tr>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+							</tr>
+						</tbody>
+					</table>
+				</td>
+				<td>
+					<table class="table">
+						<tbody class="table">
+							<tr>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+							</tr>
+							<tr>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+							</tr>
+							<tr>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+							</tr>
+						</tbody>
+					</table>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<table class="table">
+						<tbody class="table">
+							<tr>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+							</tr>
+							<tr>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+							</tr>
+							<tr>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+							</tr>
+						</tbody>
+					</table>
+				</td>
+				<td>
+					<table class="table">
+						<tbody class="table">
+							<tr>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+							</tr>
+							<tr>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+							</tr>
+							<tr>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+							</tr>
+						</tbody>
+					</table>
+				</td>
+				<td>
+					<table class="table">
+						<tbody class="table">
+							<tr>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+							</tr>
+							<tr>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+							</tr>
+							<tr>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+							</tr>
+						</tbody>
+					</table>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<table class="table">
+						<tbody class="table">
+							<tr>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+							</tr>
+							<tr>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+							</tr>
+							<tr>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+							</tr>
+						</tbody>
+					</table>
+				</td>
+				<td>
+					<table class="table">
+						<tbody class="table">
+							<tr>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+							</tr>
+							<tr>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+							</tr>
+							<tr>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+							</tr>
+						</tbody>
+					</table>
+				</td>
+				<td>
+					<table class="table">
+						<tbody class="table">
+							<tr>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+							</tr>
+							<tr>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+							</tr>
+							<tr>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+								<td><input type="text" class="cell" maxlength="1"></td>
+							</tr>
+						</tbody>
+					</table>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+</body>
+</html>
